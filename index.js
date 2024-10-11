c07798a5f64fdd442c5e58dca50405f0948a065f
dcruzel <liz.c.dcruz@gmail.com>
2024-10-11 03:01:16 -0400

    Fixed Output README File Javascript

--- index.js
+++ index.js
@@ -3,31 +3,35 @@ import newinquirer from 'inquirer';
 //import fs for writing to file
 import newfs from 'fs';
 
+//license variable string
+let pick; 
+
 //create a function to generate README file string
 const generateReadMeFile=({projectTitle, description, installInstructions, usage, contributing, tests, questions, licenseChoosen, gitHubUsername, email}) =>
-    `# 📖 ${projectTitle}
-     ## Description
-     ${description}
-     ## Table of Contents
-     - [Installation](#installation)
-     - [Usage](#usage)
-     - [License](#license)
-     - [Contributing](#contributing)
-     - [Tests](#tests)
-     - [Questions](#questions)
-     ## Installation
-     ${installInstructions}
-     ## Usage
-     ${usage}
-     ## License
-     ${licenseChoosen}
-     ## Contributing
-     ${contributing}
+`# ${projectTitle}
+${pick}
+## Description
+${description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${installInstructions}
+## Usage
+${usage}
+## License
+${pick}
+## Contributing
+${contributing}
 
-     ## Tests
-     ${tests}
-     ## Questions
-     For additional questions, please reach me at ${email} or [${gitHubUsername}](https://github.com/${gitHubUsername})`;
+## Tests
+${tests}
+## Questions
+For additional questions, please reach me at ${email} or [${gitHubUsername}](https://github.com/${gitHubUsername})`;
 
      // Get the inputs through the inquirer prompt function
     newinquirer
@@ -85,39 +89,39 @@ const generateReadMeFile=({projectTitle, description, installInstructions, usage
   ])
   .then((data) => {
     // Update the data license string for the README file
-    switch(data.licenseChosen) {
+  
+    switch(data.licenseChoosen) {
         case 'Apache':
-            data.licenseChosen='[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            pick='[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
             break;
         case 'Boost':
-            data.licenseChosen='[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+            pick='[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
             break;
         case 'BSD 3-Clause License':
-            data.licenseChosen='[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+            pick='[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
             break;
         case 'Eclipse Public License 1.0':
-            data.licenseChosen='[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+            pick='[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
             break;
         case 'ISC License':
-            data.licenseChosen='[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+            pick='[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
             break;
         case 'The MIT License':
-            data.licenseChosen='[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            pick='[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
             break;
         case 'Mozilla Public License 2.0':
-            data.licenseChosen='[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+            pick='[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
             break;
         case 'The Perl License':
-            data.licenseChosen='[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)';
+            pick='[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)';
             break;
         default:
-            data.licenseChosen='[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+            pick='[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
       }
-
 
      // Calls function to generate the readme file
     const mdPage = generateReadMeFile(data);
-    newfs.writeFile('./sample/README.md', JSON.stringify(mdPage, null, '\t'), (err) =>
+    newfs.writeFile('./sample/README.md', mdPage, (err) =>
       err ? console.log(err) : console.log('Success!')
     );
   });
--- sample/README.md
+++ sample/README.md
@@ -1,1 +1,24 @@
-"# 📖 Special Professional README File Generator\n     ## Description\n     This project will create a professional README file. \n     ## Table of Contents\n     - [Installation](#installation)\n     - [Usage](#usage)\n     - [License](#license)\n     - [Contributing](#contributing)\n     - [Tests](#tests)\n     - [Questions](#questions)\n     ## Installation\n     1. Install the fs and inquirer packages 2. Run the index.js 3. Enter the suggested information\n     ## Usage\n     None\n     ## License\n     ISC License\n     ## Contributing\n     None\n\n     ## Tests\n     None\n     ## Questions\n     For additional questions, please reach me at liz.c.dcruz@gmail.com or [dcruzel](https://github.com/dcruzel)"
+# Special Professional README File Generator
+[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+## Description
+This project will create a professional README file.
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+1. Install the fs and inquirer packages 2. Run the index.js 3. Enter the suggested information
+## Usage
+None
+## License
+[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+## Contributing
+None
+
+## Tests
+1. Check for the badge of license near the top of the README, 2. Under questions, check for link to my GitHub profile, 3. Check for links for the Table of Contents
+## Questions
+For additional questions, please reach me at liz.c.dcruz@gmail.com or [dcruzel](https://github.com/dcruzel)
